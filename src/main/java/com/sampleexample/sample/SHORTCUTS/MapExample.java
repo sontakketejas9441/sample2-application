dad78ad35a2ef2c9587db6d322d512bb2b6b4493
Tejas <newuser@10-136-62-154.wifi-m.deakin.edu.au>
2022-06-14 22:31:50 +1000

    initial commit

---
+++ src/main/java/com/sampleexample/sample/SHORTCUTS/MapExample.java
@@ -0,0 +1,41 @@
+package com.sampleexample.sample.SHORTCUTS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+    public static void main(String[] args) {
+
+        //Map Examples
+        Map<String,Integer> sampleMap = new HashMap<>();
+
+        sampleMap.put("One",1);
+        sampleMap.put("Two",2);
+
+        System.out.println("Size of Hashmap: " +sampleMap.size());
+
+        /*
+        Checking if the key is present in HashMap and
+        Then to fetch the associated value
+        */
+
+        if (sampleMap.containsKey("One")){
+            Integer value = sampleMap.get("One");
+            System.out.println("value = " + value);
+
+        }
+
+        System.out.println("=========");
+
+        /*
+        Traversing the hashmap using the for-each loop
+        */
+        for (Map.Entry<String,Integer> e:
+             sampleMap.entrySet()) {
+            System.out.println("Key = " + e.getKey());
+            System.out.println("Value = " + e.getValue());
+        }
+
+        System.out.println("=========");
+    }
+}
---
+++ src/main/java/com/sampleexample/sample/SHORTCUTS/ShortcutClass.java
@@ -0,0 +1,21 @@
+package com.sampleexample.sample.SHORTCUTS;
+
+public class ShortcutClass {
+    public static void main(String[] args) {
+
+        /*
+        Control + Shift + R = To run Program
+
+        Command + O = For search window
+        Command + 1 = Project Window
+        Command + E = TO search for recent file
+
+        Command + N = For GETTER/SETTER methods and so on....
+        Command + Option + M = To create the method out of selected
+
+        Esc = Back To code window
+        OPTION + ENTER = Suggestion
+
+        */
+    }
+}
